482f18e6e0126b5b6b0f9175c960cf4a835bc997
Attiya Riaz <48679960+Tia112@users.noreply.github.com>
2020-11-28 18:35:24 +0000

    Major Array Addition for Javascript
    Added function prevent any caching
    Added array function to hold/retrieve data for HTML modal

--- index.js
+++ index.js
@@ -29,39 +29,45 @@ document.addEventListener("keypress", function (event) {
     });
 
 /* Set to play each sound on key press using makeSound function */ 
-
+var animal_audio = null;
 function makeSound(key) {
-
-    switch (key) {
+    inital_key = key.charAt(0).toLowerCase();
+    switch (inital_key) {
         case "l":
-            var lion4 = new Audio("sounds/lion4.wav")
+            var lion4 = new Audio("sounds/lion4.wav");
             lion4.play();
-            break;l
+            animal_audio = lion4;
+            break;
         
         case "e":
             var elephant = new Audio("sounds/elephant.wav")
             elephant.play();
+            animal_audio = elephant;
             break;
 
         case "f":
             var frog = new Audio("sounds/frog.wav")
             frog.play();
+            animal_audio = frog;
             break;
 
 
         case "b":
             var bear = new Audio("sounds/bearsound.wav")
             bear.play();
+            animal_audio = bear;
             break;   
 
         case "m":
             var monkey = new Audio("sounds/monkey2.wav")
             monkey.play();
+            animal_audio = monkey;
             break;   
 
         case "h":
             var hippo = new Audio("sounds/hippo1.wav")
             hippo.play();
+            animal_audio = hippo;
             break;   
         default: console.log(key);
     }
@@ -69,26 +75,29 @@ function makeSound(key) {
 
 }
 
-//Data retrieval for Modal container test
-
-    $(document).on("click", ".open-modal", function () {
-        var myHeading = "<p>Lions are the only cats that live in groups.  A group, or pride, can be up to 30 lions, depending on how much food and water is available.  Female lions are the main hunters. While they’re out looking for food, the males guard the pride’s territory and their young. </p>";
-        var myElephant = "<p>Elephants belong to the mammal family, which means that they have hair, give birth to live young, and feed their babies milk. They have large, thin ears that are used to help cool them down, and have long, powerful trunks. </p>";
-        var myFrog = "<p>Frogs are amphibians. They can be found near water but prefer ponds, lakes, and marshes. You will not find any in Antarctica. They lay eggs called Frog Spawn. Their eggs hatch into tadpoles. A group of frogs is called an army. </p>";
-        var myBear = "<p>Koalas are not bears. The dawn bear was the first bear. In the north, bears sleep in the winter, but they don't hibernate. Bears are related to walruses, seals and sea lions.  Bears can see color. Not all mammals can.  Giant Panda are classified as a bear. </p>";
-        var myMonk = "<p>There are currently 264 known monkey species. Monkeys can be divided into two groups, Old World monkeys that live in Africa and Asia, and New World monkeys that live in South America. A baboon is an example of an Old World monkey, while a marmoset is an example of a New World monkey. </p>";
-        var myHipp = "<p>Hippo's Cannot Swim.  Shocking! They Have Incredibly Sensitive Skin. They Cannot Breathe Underwater. Hippos Are Territorial – But Only In Water. They Are Not Big Eaters. Hippos Have A British Connection. </p>";
-
-        $("#modal-body").html(myHeading);
-        $("#modal-body").html(myElephant);   
-        $("#modal-body").html(myFrog);
-        $("#modal-body").html(myBear);
-        $("#modal-body").html(myMonk);
-        $("#modal-body").html(myHipp);
-
-        $('#modal').modal('show');
-        
-        
-    });
+function stopSound(){
+    animal_audio.pause();
+    animal_audio=null;
+}
 
 
+//Data retrieval for Modal container test - using an array to hold data for Modal.  Eventually this array will hold all the data for each Animal and can be expanded to hold other elements too.
+
+function openModal(type) {
+	$('#modal').modal('show');
+	var arr = [
+	{
+ 		name : 'L',
+ 		attributes: "<p>Lions are the only cats that live in groups.  A group, or pride, can be up to 30 lions, depending on how much food and water is available.  Female lions are the main hunters. While they’re out looking for food, the males guard the pride’s territory and their young. </p>"
+	
+	}
+	] 
+
+	var obj = arr.find(function(data) {
+		return data.name === type;
+	})
+	$('#inputData').html('');
+	$('#inputData').html(obj.attributes);
+
+	
+}
